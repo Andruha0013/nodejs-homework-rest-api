b29bc3396525c6decdc1a9640371ca716beb6068
Andruha0013 <andruha0013@gmail.com>
2023-05-26 21:40:30 +0300

    all done

--- midleware/index.js
+++ midleware/index.js
@@ -1,7 +1,9 @@
 const validateBody = require("./validateBody");
 const authenticate = require("./authenticate");
+const uploder = require("./uploader");
 
 module.exports = {
 	validateBody,
 	authenticate,
+	uploder,
 };
